bcc7e0522efecb4c2edf9565d0308ef137409ba9
xcvxcv203 <xcvxcv203@gmail.com>
2025-04-18 18:51:00 +0900

    .gitignore 적용

--- Assets/Script/Car/CarController.cs
+++ Assets/Script/Car/CarController.cs
@@ -16,14 +16,33 @@ public class CarController : MonoBehaviour
 
     public float motorForce = 1500f;
     public float maxSteerAngle = 30f;
+    public float brakeForce = 3000f;
 
     private float steerInput;
     private float motorInput;
+    private float currentBrakeForce;
+    private bool isBrake;
+
+    //자동 변속 관련
+    public float[] gearRatios = { 2.5f, 2.0f, 1.5f, 1.0f, 0.75f };
+    private int currentGear = 0;
+
+    public float shiftUpRpm = 4000f;
+    public float shiftDownRpm = 1500f;
+    public float engineRpm;
+
+    public Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
         steerInput = Input.GetAxis("Horizontal");
         motorInput = Input.GetAxis("Vertical");
+        isBrake = Input.GetKey(KeyCode.Space);
 
         UpdateWheelVisual(frontLeftCollider, frontLeftTransform);
         UpdateWheelVisual(frontRightCollider, frontRightTransform);
@@ -33,11 +52,49 @@ public class CarController : MonoBehaviour
 
     void FixedUpdate()
     {
+        HandleSteer();
+        HandleMotor();
+        ApplyBrake();
+        GearShifting();
+    }
+
+    private void HandleSteer()
+    {
         frontLeftCollider.steerAngle = maxSteerAngle * steerInput;
         frontRightCollider.steerAngle = maxSteerAngle * steerInput;
+    }
 
+    private void HandleMotor()
+    {
         frontLeftCollider.motorTorque = motorForce * motorInput;
         frontRightCollider.motorTorque = motorForce * motorInput;
+        rearLeftCollider.motorTorque = motorForce * motorInput;
+        rearRightCollider.motorTorque = motorForce * motorInput;
+    }
+
+    private void ApplyBrake()
+    {
+        currentBrakeForce = isBrake ? brakeForce : 0f;
+
+        frontLeftCollider.brakeTorque = currentBrakeForce;
+        frontRightCollider.brakeTorque = currentBrakeForce;
+        rearLeftCollider.brakeTorque = currentBrakeForce;
+        rearRightCollider.brakeTorque = currentBrakeForce;
+    }
+
+    private void GearShifting()
+    {
+        float averageRpm = (frontLeftCollider.rpm + frontRightCollider.rpm + rearLeftCollider.rpm + rearLeftCollider.rpm) / 4f;
+        engineRpm = Mathf.Abs(averageRpm * gearRatios[currentGear]);
+
+        if (engineRpm > shiftUpRpm && currentGear < gearRatios.Length - 1)
+        {
+            currentGear++;
+        }
+        else if (engineRpm < shiftDownRpm && currentGear > 0)
+        {
+            currentGear--;
+        }
     }
 
     private void UpdateWheelVisual(WheelCollider collider, Transform wheelTransform)
@@ -48,4 +105,8 @@ public class CarController : MonoBehaviour
         wheelTransform.position = pos;
         wheelTransform.rotation = rot;
     }
+
+    public int GetCurrentGear() => currentGear + 1;
+    public float GetEngineRPM() => engineRpm;
+    public float GetSpeedKmh() => rb.velocity.magnitude * 3.6f;
 }
